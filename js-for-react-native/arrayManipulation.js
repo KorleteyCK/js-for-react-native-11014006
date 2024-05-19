9e681bb69a477d7f91aa11a217dcf7dbb03e73b1
KorleteyCK <152325867+KorleteyCK@users.noreply.github.com>
2024-05-19 23:56:34 +0100

    Add files via upload

---
+++ js-for-react-native/arrayManipulation.js
@@ -0,0 +1,32 @@
+// Task 1
+
+function processArray(arr) {
+  return arr.map((num) => {
+    if (num % 2 === 0) {
+      return num * num;
+    } else {
+      return num * 3;
+    }
+  });
+}
+
+const numbers = [1, 2, 3, 4, 5];
+const processedNumbers = processArray(numbers);
+console.log(processedNumbers);
+
+// Task 2
+
+export function formatArrayStrings(stringArray, numberArray) {
+  return stringArray.map((str, index) => {
+    if (numberArray[index] % 2 === 0) {
+      return str.toUpperCase();
+    } else {
+      return str.toLowerCase();
+    }
+  });
+}
+
+const stringArray = ["Apple", "Banana", "Strawberry", "Pineapple", "Pear"];
+const numberArray = processArray([1, 2, 3, 4, 5]);
+const formattedStrings = formatArrayStrings(stringArray, numberArray);
+console.log(formattedStrings);
